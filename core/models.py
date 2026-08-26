757928160d25c936ff96a9eb7d569b817db27d39
Alexsandro Barbosa da Silva <alexdeveloper2022@outlook.com>
2026-08-26 11:51:38 -0300

    API Django REST pronta para deploy

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Pessoa(models.Model):
+
+    nome = models.CharField(max_length=100)
+
+    idade = models.IntegerField()
+
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
---
+++ myapp/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from rest_framework import routers
+
+from core.views import PessoaViewSet
+
+
+router = routers.DefaultRouter()
+
+router.register('pessoas', PessoaViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+
+    path('admin/', admin.site.urls),
+
+    path('api-auth/', include('rest_framework.urls')),
+]
